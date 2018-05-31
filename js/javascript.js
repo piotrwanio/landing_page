d34857431b898ec7a32739335915b37fb90f412e
piotrwanio <34741162+piotrwanio@users.noreply.github.com>
2018-05-31 12:56:31 +0200

    added hamburger menu

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,56 @@ $arrow.on("click", function () {
         scrollTop: $("main").offset().top
     }, 1000);
 })
+
+
+$(document).ready(function () {
+
+    var menu = $("menu.active");
+    var hamburger = $("nav i");
+    var menuOpen;
+
+    function openMenu() {
+        menu.css("right", "0px");
+        hamburger.css("color", "white");
+        menuOpen = true;
+    }
+
+    function closeMenu() {
+        menu.css("right", "-90%");
+        hamburger.css("color", "black");
+        menuOpen = false;
+    }
+
+    function toggleMenu() {
+        if (menuOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    /*
+
+        hamburger.on({
+            mouseenter: function () {
+                openMenu();
+            }
+        });
+
+        menu.on({
+            mouseleave: function () {
+                closeMenu();
+            }
+
+        });
+    */
+
+    hamburger.on({
+        click: function () {
+            toggleMenu();
+        }
+    })
+
+
+
+});
